1f78afc8f1e1452fc45ac539c99d6db87dcb551d
Levy <levymcruz@gmail.com>
2023-10-13 23:39:45 -0300

    initial config of vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import {BrowserRouter as Router, Route, Link, Routes, useNavigate} from 'react-router-dom';
+
+export default function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
